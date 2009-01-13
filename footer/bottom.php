b80db9a97489042b57d9a2804432fb4e4e59b76d
Alex King <alex@crowdfavorite.com>
2009-01-13 00:39:30 +0000

    probably need a better color CF logo image. This adds styling for the About box and makes the left edge consistent

--- footer/bottom.php
+++ footer/bottom.php
@@ -21,7 +21,8 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 $about_text = cfct_about_text();
 if (!empty($about_text)) {
 ?>
-<div id="about" class="content">
+<div id="about" class="group">
+	<h3><?php printf(__('About %s', 'carrington-mobile'), get_bloginfo('name')); ?></h3>
 <?php
 	echo $about_text;
 ?>
@@ -34,7 +35,18 @@ if (!empty($about_text)) {
 
 	<hr />
 
-	<p class="small">Proudly powered by <a href="http://wordpress.org"><strong>WordPress</strong></a> and <a href="http://carringtontheme.com"><strong>Carrington</strong></a>. <a href="http://alexking.org/projects/wordpress">WordPress Mobile Edition</a> available at alexking.org.</p>
+	<p class="small">
+		Proudly powered by <a href="http://wordpress.org"><strong>WordPress</strong></a> and <a href="http://carringtontheme.com"><strong>Carrington</strong></a>.<br />
+		<a href="http://alexking.org/projects/wordpress">WordPress Mobile Edition</a> available at alexking.org.
+	</p>
+<?php
+if (cfct_get_option('cfct_credit') == 'yes') {
+?>
+	<p id="developer-link"><?php printf(__('<a href="http://crowdfavorite.com" title="Custom WordPress development, design and backup services." rel="developer designer">%s</a>', 'carrington-mobile'), 'Carrington Theme by Crowd Favorite'); ?></p>
+<?php
+}
+?>
+	<div class="clear"></div>
 </div>
 <script type="text/javascript">
 	document.write('<?php
