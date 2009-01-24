ea6e6155bd60e9c9d7596343dcf8a67ca55e3d33
Alex King <alex@crowdfavorite.com>
2009-01-24 20:19:10 +0000

    prev/next fix

--- misc/nav-list.php
+++ misc/nav-list.php
@@ -1,5 +1,5 @@
 <span>&laquo;</span>
-<span class="prev"><?php previous_posts_link('Previous'); ?></span>
+<span class="next"><?php previous_posts_link('Previous'); ?></span>
 <span>|</span>
-<span class="next"><?php next_posts_link('Next'); ?></span>
+<span class="prev"><?php next_posts_link('Next'); ?></span>
 <span>&raquo;</span>
