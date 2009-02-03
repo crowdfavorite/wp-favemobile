9eaf0404485fc11580c6542db1fe44a53a116ed9
Gordon Brander <gordon@crowdfavorite.com>
2009-02-03 17:01:34 +0000

    Added search template and proprietary webkit property that prevents text algorithm from borking.

---
+++ posts/search.php
@@ -0,0 +1,40 @@
+<?php
+
+// This file is part of the Carrington Mobile Theme for WordPress
+// http://carringtontheme.com
+//
+// Copyright (c) 2008-2009 Crowd Favorite, Ltd. All rights reserved.
+// http://crowdfavorite.com
+//
+// Released under the GPL license
+// http://www.opensource.org/licenses/gpl-license.php
+//
+// **********************************************************************
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// **********************************************************************
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+get_header();
+
+$s = get_query_var('s');
+
+$search_title = '<a href="'.get_bloginfo('url').'/?s='.attribute_escape($s).'" title="">'.htmlspecialchars($s).'</a>';
+
+?>
+
+<div id="content">
+	<h1 class="table-title"><?php printf(__('Search Results for: %s', 'carrington-blog'), $search_title); ?></h1>
+<?php
+	cfct_loop();
+?>
+</div><!--#content-->
+
+<?php
+
+get_footer();
+
+?>
